bf56a7210686dfabf4ebdcd0d703e5955bb80a64
studenti <studenti@rosselli.loc>
2020-02-25 09:36:49 +0100

    Add project files.

---
+++ ConsoleBubbleSort/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace ConsoleBubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensione = { 1, 20, 50, 100, 500, 1000 };
+            using (StreamWriter sw = new StreamWriter("Stat.csv", false, Encoding.UTF8))
+            {
+                sw.Write("Algoritmo ; Dimensione ; Tipo");
+                foreach (int dim in dimensione)
+                {
+                    int[] numeri = new int[dim];
+                    Random r = new Random();
+                    for (int i = 0; i < dim; i++)
+                    {
+                        numeri[i] = r.Next();
+                    }
+                    Stopwatch s = new Stopwatch();
+                    s.Start();
+                    LibraryBubbleSort.bubblesort1.Sort(numeri);
+                    s.Stop();
+                    long elapsed = s.ElapsedMilliseconds;
+                    sw.WriteLine($"Bubblesort;{dim};{elapsed}");
+                }
+                sw.Flush();
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ LibraryBubbleSort/insertSort.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryBubbleSort
+{
+    class insertSort
+    {
+        public static void ShiftR(int[] array)
+        {
+            for(int i = 1; i < array.Length; i++)
+            {
+                int n = array[i];
+                int j = i - 1;
+                while (j >= 0 && array[j] > n)
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j + 1] = n;
+            }
+        }
+    }
+}
